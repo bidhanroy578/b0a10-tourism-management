4725d65d9594151bc50992d08e989c503d8a4bab
Bidhan Roy <bidhanroy578@gmail.com>
2025-02-26 07:48:32 +0600

    countries res

--- client/src/pages/home/Countries.jsx
+++ client/src/pages/home/Countries.jsx
@@ -7,7 +7,7 @@ const Countries = () => {
         <div>
             <h3 className="text-5xl text-center my-10">All Listed Countries</h3>
 
-            <div className="grid grid-cols-4 justify-self-center gap-8">
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-self-center gap-8">
                 {/* card 1 */}
                 <div className="w-60 h-72 bg-cover bg-center" style={{backgroundImage : `url(${image})`}}>
                     <div className="font-semibold text-xl text-black h-full text-center content-center hover:backdrop-blur-[2px]">
