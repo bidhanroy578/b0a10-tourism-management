848b648c55fbc094d84b99ffb16f7b284e2f0950
Bidhan Roy <bidhanroy578@gmail.com>
2025-02-26 17:40:54 +0600

    added all spots page

---
+++ client/src/pages/allspots/AllSpots.jsx
@@ -0,0 +1,42 @@
+
+const AllSpots = () => {
+
+    const allSpots = [
+        { "name": "Angkor Wat", "image": "https://www.goworldtravel.com/wp-content/uploads/2023/02/angkor-wat-travel.jpg" },
+        { "name": "Angkor Wat", "image": "https://www.goworldtravel.com/wp-content/uploads/2023/02/angkor-wat-travel.jpg" },
+        { "name": "Angkor Wat", "image": "https://www.goworldtravel.com/wp-content/uploads/2023/02/angkor-wat-travel.jpg" },
+        { "name": "Angkor Wat", "image": "https://www.goworldtravel.com/wp-content/uploads/2023/02/angkor-wat-travel.jpg" },
+        { "name": "Angkor Wat", "image": "https://www.goworldtravel.com/wp-content/uploads/2023/02/angkor-wat-travel.jpg" },
+        { "name": "Angkor Wat", "image": "https://www.goworldtravel.com/wp-content/uploads/2023/02/angkor-wat-travel.jpg" },
+        { "name": "Angkor Wat", "image": "https://www.goworldtravel.com/wp-content/uploads/2023/02/angkor-wat-travel.jpg" },
+        { "name": "Angkor Wat", "image": "https://www.goworldtravel.com/wp-content/uploads/2023/02/angkor-wat-travel.jpg" },
+    ]
+
+    return (
+        <div className="flex flex-auto flex-wrap gap-5 justify-center w-full my-16">
+            {
+                allSpots.map((spot, index) => (
+                    <div key={index} className="card card-side shadow-blue-300 shadow-lg ">
+                        <figure className='flex-1/2 max-w-sm object-cover'>
+                            <img src={spot.image} />
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title">{spot.name}</h2>
+                            <p>average cost here</p>
+                            <p><span> 10000/year </span> <span> 7 days plan </span> </p>
+                            <div className="card-actions">
+                                <div className="badge badge-outline">summer</div>
+                                <div className="badge badge-outline">winter</div>
+                            </div>
+                            <div className="card-actions">
+                                <button className="btn w-full bg-blue-400">Details</button>
+                            </div>
+                        </div>
+                    </div>
+                ))
+            }
+        </div>
+    );
+};
+
+export default AllSpots;
--- client/src/router.jsx
+++ client/src/router.jsx
@@ -3,25 +3,30 @@ import App from "./App";
 import Login from "./pages/authentication/Login";
 import Register from "./pages/authentication/Register";
 import Home from "./pages/home/Home";
+import AllSpots from "./pages/allspots/AllSpots";
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <App />,
-      children: [
-        {
-path: "/",
-element: <Home />,
-        },
-        {
-          path: "/login",
-          element: <Login />,
-        },
-        { 
-          path: "/register",
-          element: <Register />,
-        }  
-        ],
-    },
-  ]);
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: '/all-spots' ,
+        element: <AllSpots />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      }
+    ],
+  },
+]);
 export default router;
