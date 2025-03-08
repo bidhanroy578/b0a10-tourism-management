7e8a32014f092d6d2489ea098b5c9f3b9480da75
Bidhan Roy <bidhanroy578@gmail.com>
2025-03-08 17:03:24 +0600

    added register functionality and sweer alert

--- client/src/pages/authentication/Register.jsx
+++ client/src/pages/authentication/Register.jsx
@@ -1,55 +1,89 @@
 // import { useContext } from "react";
-import { Link } from "react-router";
+import { Link, useLocation, useNavigate } from "react-router";
 import bg2 from './../../assets/svg/bg2.svg';
+import { useContext } from "react";
+import { AuthContext } from "../../providers/Contexts";
+import { updateProfile } from "firebase/auth";
+import Swal from "sweetalert2";
 // import { AuthContext } from "../../context_provider/AuthContext";
 // import { updateProfile } from "firebase/auth";
 // import auth from "../../firebase/__config__";
 
 const Register = () => {
-    // const { registerWithEmail, loginWithGoogle } = useContext(AuthContext)
 
-    // const handleGoogleLogin = () => {
-    //     loginWithGoogle()
-    //         .then(result => {
-    //             console.log(result.user)
-    //             alert('User logged in successfully with Google')
-    //         }).catch(err => {
-    //             console.error(err);
-    //             alert(err.message)
-    //         })
-    // }
-    // const handleSubmit = (e) => {
-    //     e.preventDefault();
-    //     const form = e.target;
-    //     const name = form.name.value;
-    //     const email = form.email.value;
-    //     const password = form.password.value;
-    //     console.log(email, password, name);
-    //     registerWithEmail(email, password)
-    //         .then(result => {
-    //             console.log(result.user)
-    //             updateProfile(result.user, {
-    //                 displayName: name,
-    //             })
-    //                 .then(() => {
-    //                     alert('User registration successful')
-    //                 }).catch((error) => {
-    //                     console.error(error);
-    //                     alert(error.message + 'from update ')
-    //                 })
-    //         }).catch(err => {
-    //             console.error(err);
-    //             alert(err.message + 'from register')
-    //         })
-    // }
+    const location = useLocation()
+    const navigate = useNavigate()
+    const { registerWithEmail, loginWithGoogle, } = useContext(AuthContext)
+
+    function successAlert() {
+        Swal.fire({
+            title: "Successfull!",
+            text: "Your account registered successfully",
+            icon: "success",
+            confirmButtonColor: "#01f702",
+            confirmButtonText: "Ok"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                navigate(location?.state || '/')
+            }
+        });
+    }
+    function errorAlert(errorMessage) {
+        Swal.fire({
+            title: "Invalid email or password!!",
+            text: errorMessage,
+            icon: "error",
+            confirmButtonColor: "#f60002",
+            confirmButtonText: "Ok"
+        });
+    }
+
+    const handleGoogleLogin = () => {
+        loginWithGoogle()
+            .then(result => {
+                console.log(result.user)
+                successAlert()
+            }).catch(err => {
+                console.error(err);
+                errorAlert(err.message)
+            })
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password, name);
+        if (password.length < 6) {
+            errorAlert('password must be at least 6 characters')
+            return
+        }
+        registerWithEmail(email, password)
+            .then(result => {
+                console.log(result.user)
+                updateProfile(result.user, {
+                    displayName: name,
+                })
+                    .then(() => {
+                        successAlert()
+                    }).catch((error) => {
+                        console.error(error);
+                        errorAlert(error.message)
+                    })
+            }).catch(err => {
+                console.error(err);
+                errorAlert(err.message)
+            })
+    }
     return (
-        <div className="hero min-h-screen bg-cover bg-center" style={{backgroundImage : `url(${bg2})`}}>
-                <div className="hero-content backdrop-blur-[3px] flex-col w-full max-w-md">
+        <div className="hero min-h-screen bg-cover bg-center" style={{ backgroundImage: `url(${bg2})` }}>
+            <div className="hero-content backdrop-blur-[3px] flex-col w-full max-w-md">
                 <div className="text-center">
                     <h1 className="text-5xl font-bold">Register Now!</h1>
                 </div>
                 <div className="card w-full max-w-sm shrink-0 shadow-2xl">
-                    <form className="card-body">
+                    <form onSubmit={handleSubmit} className="card-body">
                         <fieldset className="fieldset">
                             <label className="fieldset-label text-black">Name</label>
                             <input type="text" name="name" className="input" placeholder="Name" />
@@ -61,7 +95,7 @@ const Register = () => {
                             <p className="text-white">Already have an account? <Link to='/login' className="link hover:text-red-400 font-rancho text-[15px]">login here</Link></p>
                         </fieldset>
                     </form>
-                    <button className="btn bg-amber-200 text-black mt-4">Sign In With Google</button>
+                    <button onClick={handleGoogleLogin} className="btn bg-amber-200 text-black mt-4">Sign In With Google</button>
                 </div>
             </div>
         </div>
