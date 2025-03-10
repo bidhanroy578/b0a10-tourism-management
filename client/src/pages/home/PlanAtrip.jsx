7525fac6deafa6f03066026135df5160ee276536
Bidhan Roy <bidhanroy578@gmail.com>
2025-03-10 20:38:19 +0600

    made some design correction

--- client/src/pages/home/PlanAtrip.jsx
+++ client/src/pages/home/PlanAtrip.jsx
@@ -2,9 +2,9 @@ import bg6 from '../../assets/svg/bg6.svg'
 const PlanAtrip = () => {
     return (
         <div>
-            <div className='flex w-full justify-self-center items-center overflow-x-hidden'>
-                <h3 className='text-[4vw]'>Plan a trip for your Desired Destination with Us</h3>
-                <img src={bg6} className='flex-1/2 max-h-screen'/>
+            <div className='flex w-full justify-self-center items-center p-2'>
+                <h3 className='text-[4vw] min-w-[45vw]'>Plan a trip for your Desired Destination with Us</h3>
+                <img src={bg6} className='flex-1/2 max-w-[45vw]' />
             </div>
         </div>
     );
